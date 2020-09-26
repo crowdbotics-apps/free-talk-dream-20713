fd3ddabf29233137497e7c2a87f62c281aa15854
Crowdbotics <team@crowdbotics.com>
2020-09-26 04:01:14 +0000

    Initial commit.

---
+++ ios/free_talk_dream_20713Tests/free_talk_dream_20713Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface free_talk_dream_20713 : XCTestCase
+
+@end
+
+@implementation free_talk_dream_20713
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
